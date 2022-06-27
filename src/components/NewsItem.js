9971216362fd6cf0b837c636dc50bd4e67c6439d
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2022-06-27 20:25:09 +0530

    added state for news card

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 export class NewsItem extends Component {
-  static propTypes = {
-
-  }
-
+  
   render() {
 
-    let {title,description}=this.props;
+    let {title,description,imageUrl}=this.props;
 
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-            <img src="..." className="card-img-top" alt="..."/>
+            <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href="/#" className="btn btn-primary">Go somewhere</a>
+                <a href="/#" className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
       </div>
