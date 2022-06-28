26e583e7d824b1dc6884a076e602915c04c14ef5
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2022-06-28 13:43:14 +0530

    aligned button

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,10 +10,10 @@ export class NewsItem extends Component {
       <div>
         <div className="card my-3" style={{width: "18rem"}}>
             <img src={imageUrl?imageUrl:"https://cdn.w600.comps.canstockphoto.com/breaking-news-world-icon-image_csp66446979.jpg"} className="card-img-top" alt="..." height="175" />
-            <div className="card-body">
+            <div className="d-flex flex-column card-body" style={{height: "14rem"}}>
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
-                <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read more</a>
+                <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary mt-auto">Read more</a>
             </div>
         </div>
       </div>
