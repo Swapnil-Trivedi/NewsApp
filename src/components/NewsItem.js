56851d3774138dd1cd26f734d482bce2dc127205
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2022-06-28 14:21:38 +0530

    added next and prev parameter

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -13,7 +13,7 @@ export class NewsItem extends Component {
             <div className="d-flex flex-column card-body" style={{height: "14rem"}}>
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
-                <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary mt-auto">Read more</a>
+                <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark mt-auto">Read more</a>
             </div>
         </div>
       </div>
--- src/components/Newscard.js
+++ src/components/Newscard.js
@@ -8,19 +8,42 @@ export class Newscard extends Component {
         super();
         this.state={
             articles: [],
-            loading: false
+            loading: false,
+            page:1,
         }
         
       }
     //using compenet mount to fetch
     async componentDidMount(){
-        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=ef5afa752aee46bc9b91d8aca3cdbbb8";
+        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=ef5afa752aee46bc9b91d8aca3cdbbb8&page=1&pageSize=20";
         let data= await fetch(url);
         let parsedData=await data.json();
-        console.log(parsedData);
-        this.setState({articles: parsedData.articles});
+        this.setState({articles: parsedData.articles,totalResults: parsedData.totalResults});
 
     }
+  
+    handleNext = async () => {
+        if(this.state.page+1> Math.ceil(this.state.totalResults/20)){
+            alert("Nothing more is happening around now!");
+        }
+        else{
+            
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=ef5afa752aee46bc9b91d8aca3cdbbb8&page=${this.state.page+1}&pageSize=20`;
+        let data= await fetch(url);
+        let parsedData=await data.json();
+        this.setState({articles: parsedData.articles,page: this.state.page+1});
+        }
+    }
+    handlePrevious = async () => {
+        
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=ef5afa752aee46bc9b91d8aca3cdbbb8&page=${this.state.page+1}&pageSize=20`;
+        let data= await fetch(url);
+        let parsedData=await data.json();
+        this.setState({articles: parsedData.articles,page: this.state.page+1});
+        
+    }
+    
+    
 
   render() {
     return (
@@ -35,9 +58,12 @@ export class Newscard extends Component {
             <div className="col-md-3" key={ele.url} >
                 <NewsItem title={ele.title?ele.title.slice(0,45):""} description={ele.description?ele.description.slice(0,88):""} imageUrl={ele.urlToImage} newsUrl={ele.url}/>
             </div>
-            )
-
+            )   
         })}
+            <div className="container d-flex justify-content-between">
+                <button className="btn btn-dark" disabled={this.state.page<1} onClick={this.handlePrevious}>&larr; Previous</button>
+                <button className="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
+            </div>
             
         </div>
       </div>
