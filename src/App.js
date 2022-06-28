bab68cf6a24963b5369781a405715d9615f13f99
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2022-06-28 17:48:09 +0530

    used pageSize as prop

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
     return (
       <>
       <Navbar/>
-      <Newscard/>
+      <Newscard pageSize={8}/>
       </>
     )
   }
--- src/components/Newscard.js
+++ src/components/Newscard.js
@@ -15,7 +15,7 @@ export class Newscard extends Component {
       }
     //using compenet mount to fetch
     async componentDidMount(){
-        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=ef5afa752aee46bc9b91d8aca3cdbbb8&page=1&pageSize=20";
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=ef5afa752aee46bc9b91d8aca3cdbbb8&page=1&pageSize=${this.props.pageSize}`;
         this.setState({loading:true});
         let data= await fetch(url);
         let parsedData=await data.json();
@@ -24,12 +24,12 @@ export class Newscard extends Component {
     }
   
     handleNext = async () => {
-        if(this.state.page+1> Math.ceil(this.state.totalResults/20)){
+        if(this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize)){
             alert("Nothing more is happening around now!");
         }
         else{
             
-        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=ef5afa752aee46bc9b91d8aca3cdbbb8&page=${this.state.page+1}&pageSize=20`;
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=ef5afa752aee46bc9b91d8aca3cdbbb8&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
         this.setState({loading:true});
         let data= await fetch(url);
         let parsedData=await data.json();
@@ -38,7 +38,7 @@ export class Newscard extends Component {
     }
     handlePrevious = async () => {
         
-        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=ef5afa752aee46bc9b91d8aca3cdbbb8&page=${this.state.page-1}&pageSize=20`;
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=ef5afa752aee46bc9b91d8aca3cdbbb8&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
         this.setState({loading:true});
         let data= await fetch(url);
         let parsedData=await data.json();
